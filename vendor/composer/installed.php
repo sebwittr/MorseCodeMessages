226c2a1df11316908da4ac2b2d8398e34d581414
Sebastian Wittrock <sebastianwittrock927@gmail.com>
2021-02-21 07:51:06 -0500

    composer init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '76b41699c848d89f4a1a5f128f9c090d3b51c039',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '76b41699c848d89f4a1a5f128f9c090d3b51c039',
+    ),
+    'twilio/sdk' => 
+    array (
+      'pretty_version' => '6.17.0',
+      'version' => '6.17.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b77446cc36b813122ea888d455f015b2f3ff8ba1',
+    ),
+  ),
+);
